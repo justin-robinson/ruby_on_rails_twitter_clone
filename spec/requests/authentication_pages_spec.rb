53923c001dc6fe6f0bb2f902ba815d3d5669a41f
Justin Robinson <jrobinson@scires.com>
2013-10-21 08:18:23 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -42,6 +42,14 @@ describe "Authentication" do
   		it { should have_link('Sign out',   href: signout_path) }
   		it { should_not have_link('Sign in',href: signin_path) }
 
+      describe "visit signup page" do
+        before{ visit signup_path }
+        it { should have_selector('h1', 'Sample App') }
+        it { should have_title(full_title('')) }
+      end
+
+      describe "visit"
+
   		describe "followed by signout" do
   			before { click_link "Sign out" }
   			it { should have_link('Sign in') }
@@ -61,10 +69,37 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+
         describe "after signing in" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
