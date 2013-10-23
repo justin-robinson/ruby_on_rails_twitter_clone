741d434af4bedacc9d795b4c5a1e2cc4d253832b
Justin Robinson <jrobinson@scires.com>
2013-10-23 13:11:14 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -82,6 +82,18 @@ describe "Authentication" do
           end
         end
 
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+
         describe "after signing in" do
 
           it "should render the desired protected page" do
@@ -131,6 +143,16 @@ describe "Authentication" do
             before { delete user_path(user) }
             specify { expect(response).to redirect_to(root_url) }
           end
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
         end
       end
     end
